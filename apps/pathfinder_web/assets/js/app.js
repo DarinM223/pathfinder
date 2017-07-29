1362da1cde1c032ba5277865e2c43b72e0a731d3
Darin Minamoto <darinm223@gmail.com>
2017-07-28 22:49:07 -0700

    Sets up mobx dependencies

--- apps/pathfinder_web/assets/js/app.js
+++ apps/pathfinder_web/assets/js/app.js
@@ -20,19 +20,62 @@ import "phoenix_html"
 
 // import socket from "./socket"
 
-import React, { PropTypes } from 'react'
+import React, { Component, PropTypes } from 'react'
 import ReactDOM from 'react-dom'
+import {observable, computed} from 'mobx'
+import {observer} from 'mobx-react'
 
-function HelloWorld (props) {
-  return (
-    <h1>Hello!</h1>
-  )
+class Todo {
+  id = Math.random()
+  @observable title
+  @observable finished = false
+
+  constructor (title) {
+    this.title = title
+  }
 }
 
-HelloWorld.propTypes = {
+class TodoList {
+  @observable todos = []
+
+  @computed get unfinishedTodoCount () {
+    return this.todos.filter(todo => !todo.finished).length
+  }
 }
+
+@observer
+class TodoListView extends Component {
+  render () {
+    return (
+      <div>
+        <ul>
+          {this.props.todoList.todos.map(todo =>
+            <TodoView todo={todo} key={todo.id} />
+          )}
+        </ul>
+
+        Tasks left: {this.props.todoList.unfinishedTodoCount}
+      </div>
+    )
+  }
+}
+
+const TodoView = observer(({todo}) =>
+  <li>
+    <input
+      type="checkbox"
+      checked={todo.finished}
+      onClick={() => todo.finished = !todo.finished}
+    />{todo.title}
+  </li>
+)
+
+const store = new TodoList()
+
+store.todos.push(new Todo("Item 1"))
+store.todos.push(new Todo("Item 2"))
 
 ReactDOM.render(
-  <HelloWorld/>,
+  <TodoListView todoList={store} />,
   document.getElementById('game')
 )
