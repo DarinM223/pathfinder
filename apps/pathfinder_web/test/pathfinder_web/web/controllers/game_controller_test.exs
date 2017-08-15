4543053a1595503191901ec383cd928dade06f00
Darin Minamoto <darinm223@gmail.com>
2017-08-15 15:48:56 -0700

    More game controller tests

--- apps/pathfinder_web/test/pathfinder_web/web/controllers/game_controller_test.exs
+++ apps/pathfinder_web/test/pathfinder_web/web/controllers/game_controller_test.exs
@@ -46,8 +46,8 @@ defmodule PathfinderWeb.Web.GameControllerTest do
 
   @tag login_as: "bob"
   test ":index doesn't show won games", %{conn: conn, user: user} do
-    {:ok, _} = insert_game(user, %{other_user_name: "joe", other_user_type: "nonexisting"})
-    {:ok, game} = insert_game(user, %{other_user_name: "sarah", other_user_type: "nonexisting"})
+    {:ok, _} = insert_game(user, %{other_user_name: "joe"})
+    {:ok, game} = insert_game(user, %{other_user_name: "sarah"})
 
     {:ok, _} = Data.update_game(game, %{winner: -1})
 
@@ -83,10 +83,102 @@ defmodule PathfinderWeb.Web.GameControllerTest do
   end
 
   @tag login_as: "bob"
-  test ":create fails, does not create game, and redirects", %{conn: conn, user: user} do
+  test ":create fails, does not create game, and redirects", %{conn: conn, user: _} do
     attrs = %{other_user_name: nil, other_user_type: nil}
 
     conn = post conn, game_path(conn, :create, game: attrs)
     assert html_response(conn, 200) =~ ~r/can&#39;t be blank/
+  end
+
+  @tag login_as: "bob"
+  test ":new shows recently played with usernames", %{conn: conn, user: user} do
+    {:ok, _} = insert_game(user, %{other_user_name: "other1"})
+    {:ok, _} = insert_game(user, %{other_user_name: "other2"})
+
+    conn = get conn, game_path(conn, :new)
+    assert html_response(conn, 200) =~ "other1"
+    assert String.contains?(conn.resp_body, "other2")
+  end
+
+  @tag login_as: "bob"
+  test ":delete successfully deletes game created by logged in player", %{conn: conn, user: user} do
+    {:ok, game} = insert_game(user, %{other_user_name: "dave"})
+    before_count = count_games()
+
+    conn = delete conn, game_path(conn, :delete, game.id)
+    assert redirected_to(conn) == game_path(conn, :index)
+    assert get_flash(conn, :info) == "Game deleted successfully"
+
+    after_count = count_games()
+    assert after_count == before_count - 1
+  end
+
+  @tag login_as: "bob"
+  test ":delete successfully deletes game created by other player", %{conn: conn, user: _} do
+    {:ok, other} = insert_user(%{username: "dave"})
+    {:ok, game} = insert_game(other, %{other_user_name: "bob", other_user_type: "existing"})
+    before_count = count_games()
+
+    conn = delete conn, game_path(conn, :delete, game.id)
+    assert redirected_to(conn) == game_path(conn, :index)
+    assert get_flash(conn, :info) == "Game deleted successfully"
+
+    after_count = count_games()
+    assert after_count == before_count - 1
+  end
+
+  @tag login_as: "bob"
+  test ":delete fails and redirects if user is not a player in the game", %{conn: conn, user: _} do
+    {:ok, other} = insert_user(%{username: "dave"})
+
+    {:ok, game} = insert_game(other, %{other_user_name: "sarah"})
+
+    conn = delete conn, game_path(conn, :delete, game.id)
+    assert redirected_to(conn) == game_path(conn, :index)
+    assert get_flash(conn, :error) == "Invalid game id"
+  end
+
+  @tag login_as: "bob"
+  test ":finish successfully sets winner of the other player", %{conn: conn, user: user} do
+    {:ok, other} = insert_user(%{username: "dave"})
+
+    {:ok, game1} = insert_game(other, %{other_user_name: "bob", other_user_type: "existing"})
+    {:ok, game2} = insert_game(user, %{other_user_name: "dave", other_user_type: "existing"})
+
+    next_conn = post conn, game_path(conn, :finish, game1.id)
+    assert redirected_to(next_conn) == game_path(conn, :index)
+    assert get_flash(next_conn, :info) == "Game forfeited successfully"
+
+    next_conn = post conn, game_path(conn, :finish, game2.id)
+    assert redirected_to(next_conn) == game_path(conn, :index)
+    assert get_flash(next_conn, :info) == "Game forfeited successfully"
+  end
+
+  @tag login_as: "bob"
+  test ":finish fails and redirects if user is not a player in the game", %{conn: conn, user: _} do
+    {:ok, other} = insert_user(%{username: "dave"})
+
+    {:ok, game} = insert_game(other, %{other_user_name: "sarah"})
+
+    conn = post conn, game_path(conn, :finish, game.id)
+    assert redirected_to(conn) == game_path(conn, :index)
+    assert get_flash(conn, :error) == "Invalid game id"
+  end
+
+  @tag login_as: "bob"
+  test ":show fails if user doesn't own game", %{conn: conn, user: _} do
+    {:ok, other} = insert_user(%{username: "dave"})
+    {:ok, game} = insert_game(other, %{other_user_name: "sarah"})
+
+    assert_error_sent :not_found, fn ->
+      get conn, game_path(conn, :show, game.id)
+    end
+  end
+
+  defp count_games() do
+    import Ecto.Query
+    PathfinderWeb.Repo.one(
+      from g in PathfinderWeb.Data.Game, select: count(g.id)
+    )
   end
 end
